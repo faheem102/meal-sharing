da5ed846cd8b1c97da27c9f42ee4a018e3d9ea69
faheem102 <faheemhassan1985@gmail.com>
2022-04-28 04:15:46 +0200

    mealsharing homework

---
+++ src/client/FooterComponents/Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+export function FooterSection() {
+  return (
+    <footer className="bottom-section">
+      <div>
+        <h3>Get in Touch</h3>
+      </div>
+      <div className="contact-link">
+        <a href="" className="footer-link">
+          Facebook
+        </a>
+        <a href="" className="footer-link">
+          Instagram
+        </a>
+        <a href="" className="footer-link">
+          Email
+        </a>
+        <a href="" className="footer-link">
+          Phone
+        </a>
+      </div>
+    </footer>
+  );
+}
